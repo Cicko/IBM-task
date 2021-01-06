bc24c8c3ea8ee611bfcbb1e8dac8ef5b089eeb8c
Rudolf Cicko <rudolfcickodev@gmail.com>
2021-01-06 21:00:59 +0100

    IBM-4: #5 importing default styles for react-calendar

---
+++ src/lib/components/CalendarGroup/CalendarGroup.test.tsx
@@ -0,0 +1,7 @@
+import CalendarGroup from './CalendarGroup'
+
+describe('CalendarGroup Component', () => {
+    it ('should display "No data" if there are no dates or empty array passed as props', () => {
+        
+    })
+})
---
+++ src/lib/components/CalendarGroup/CalendarGroup.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import { Empty } from 'antd'
+import Calendar from 'react-calendar'
+
+export interface CalendarGroupProps {
+    dates: Array<Date>
+    locale: string
+}
+
+/**
+ * @description This component is used to display all dates that are passed as props in a container
+ * 
+ * @param props : CalendarGroupProps
+ */
+const CalendarGroup: React.FC<CalendarGroupProps> = (props) => {
+    const { dates, locale } = props
+
+    const renderMonth = (date: Date) => (
+        <Calendar className="calendar" value={date} key={date.toDateString()} locale={locale}/>
+    )
+
+    if (dates.length === 0) {
+        return <Empty description="No dates provided"/>
+    }
+
+    return (
+        <div className="calendar-group">
+            {dates.map(renderMonth)}
+        </div>
+    )
+}
+
+export default CalendarGroup
